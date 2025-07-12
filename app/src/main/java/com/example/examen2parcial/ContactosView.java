83004eeeda5f9fd355482b8bfc2324501c9e8b83
Jose-V2 <jose.vm139@gmail.com>
2025-07-12 16:34:30 -0600

    Interface

---
+++ app/src/main/java/com/example/examen2parcial/ContactosView.java
@@ -0,0 +1,34 @@
+package com.example.examen2parcial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ContactosView extends AppCompatActivity {
+
+    Button btnatras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contactos_view);
+
+        btnatras = (Button) findViewById(R.id.btnatras);
+
+        btnatras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
